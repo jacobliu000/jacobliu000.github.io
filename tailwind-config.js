eea9c42f24ba9e1b6fd4c973cdc82f7d68b91564
CornOnTheCob <136200440+bluesiarra@users.noreply.github.com>
2025-09-27 18:15:43 -0700

    first blogpost

---
+++ tailwind-config.js
@@ -0,0 +1,10 @@
+    tailwind.config = {
+        theme: {
+            extend: {
+                colors: {
+                    mainbackground : "#161616ff",
+                    textcol: "#9ca3af",
+                }
+            }
+        }
+    }
